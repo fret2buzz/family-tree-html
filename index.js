0fe9cf6406a25d250b92ba5be9576e15799eb849
fret2buzz <fret2buzz@hotmail.com>
2020-12-12 18:56:19 +0200

    refactoring

--- index.js
+++ index.js
@@ -39,92 +39,69 @@ Colors.random = function() {
     return this.names[Math.floor(Math.random() * this.names.length)];
 };
 
+function addChildren(children, $container, parent) {
+    let color = Colors.random();
+    $container.append(`<ul style="--stroke-color: ${color};"></ul>`);
+    children.forEach((child) => {
+        let $childrenCont = $('#' + parent + ' > ul');
+        let $childCont = $('#' + child);
+        $childrenCont.append($childCont);
+    });
+};
+
 const container = $('#app');
 
-function buildParent(container, name) {
-    // console.log(name);
-    if (data.people) {
-        let fullname = data.people[name].fullname ? data.people[name].fullname : '';
-        let dateBorn = data.people[name].born ? '*' + data.people[name].born : '';
-        let dateDied = data.people[name].died ? '&#10013;' + data.people[name].born : '';
-        let sep = data.people[name].born && data.people[name].died ? '&mdash;' : '';
-        var parentItem = `
-            <div class="parents-item" data-id="${name}">
-                <div class="parents-item-name">${data.people[name].name}</div>
-                <div class="parents-item-fullname">${fullname}</div>
-                <div class="parents-item-dates">${dateBorn} ${sep} ${dateDied}</div>
-            </div>
-        `;
-    } else {
-        var parentItem = `<div class="parents-item" data-id="${name}">${name}</div>`;
-    }
-
-    container.append(parentItem);
+for (const name in data.people) {
+    // console.log(`${name}: ${data.people[name]}`);
+    let fullname = data.people[name].fullname ? data.people[name].fullname : '';
+    let dateBorn = data.people[name].born ? '*' + data.people[name].born : '';
+    let dateDied = data.people[name].died ? '&#10013;' + data.people[name].born : '';
+    let sep = data.people[name].born && data.people[name].died ? '&mdash;' : '';
+    let ul = `
+        <ul class="flow-dendrogram">
+            <li id="${name}">
+                <div class="parents couple">
+                    <div class="parents-item" data-id="${name}">
+                        <div class="parents-item-name">${data.people[name].name}</div>
+                        <div class="parents-item-fullname">${fullname}</div>
+                        <div class="parents-item-dates">${dateBorn} ${sep} ${dateDied}</div>
+                    </div>
+                </div>
+            </li>
+        </ul>`;
+    container.append(ul);
 }
-
-function buildItem($container, el) {
-    if (el.length) {
-        let color = Colors.random();
-        $container.append(`<ul style="--stroke-color: ${color};"></ul>`);
-        let $children = $container.find(' > ul');
-        el.forEach((child) => {
-            let $existing = $('[data-id="'+child+'"]').parents('li');
-            let $existingUl = $('[data-id="'+child+'"]').parents('ul');
-            if ($existing.length) {
-                $existing.attr('id', child);
-                $children.append($existingUl.html());
-                $existingUl.remove();
-            } else {
-                $children.append(`<li id="${child}"></li>`);
-                $('#' + child).append('<div class="parents"></div>');
-                let $parentsCont = $('#' + child + ' > .parents');
-                buildParent($parentsCont, child);
-            }
-        });
-    }
-};
 
 data.families.forEach((el) => {
     let $father = $('#' + el.parents[0]);
-    let mother = el.parents[1];
+    let $mother = $('#' + el.parents[1]);
 
-    if ($father.length) {
-        buildItem($father, el.children);
-        if (mother) {
-            let motherCont = $father.find(' > .parents');
-            motherCont.addClass('couple');
-            buildParent(motherCont, mother);
+    let hasChildren = $('#' + el.parents[0] + ' > ul').length;
+    if (!hasChildren) {
+        let $fatherCont = $father.find(' > .parents');
+        let $motherItem = $mother.find('.parents-item');
+        $fatherCont.append($motherItem);
+
+        if (el.children.length) {
+            addChildren(el.children, $father, el.parents[0]);
         }
-
-        if (el.parents2) {
-            $father.append('<span class="line"></span>');
-            $father.after(`<li id="${el.parents2[1]}" class="parent2"><div class="parents"></div><span class="line"></span></li>`);
-            let $parentsCont = $('#' + el.parents2[1] + ' > .parents');
-            buildParent($parentsCont, el.parents2[1]);
-            buildItem($('#' + el.parents2[1]), el.children2);
-        }
-
     } else {
-        let ul = `<ul class="flow-dendrogram" id="${el.key}"><li><div class="parents couple"></div></li></ul>`;
-        container.append(ul);
-        let $li = $('#'+el.key + '> li');
-        let $parentsCont = $('#'+el.key + '> li > .parents');
-        el.parents.forEach((parent) => {
-            buildParent($parentsCont, parent);
-        });
-        buildItem($li, el.children);
+        let line = '<span class="line"></span>';
+        $father.append(line);
+        $mother.addClass('parent2').append(line);
+        $father.after($mother);
 
-        if(el.parents2) {
-            let $li = $('#'+el.key + '> li');
-            $li.append('<span class="line"></span>');
-            $li.after('<li class="parent2"><div class="parents"></div><span class="line"></span></li>');
-            let $parentsCont = $('#'+el.key + '> li.parent2 .parents');
-            buildParent($parentsCont, el.parents2[1]);
-            buildItem($('#'+el.key + '> li.parent2'), el.children2);
+        if (el.children.length) {
+            addChildren(el.children, $mother, el.parents[1]);
         }
     }
 });
 
+$('.flow-dendrogram').each(function(index) {
+    if ($(this).find('.parents-item').length == 0) {
+        $(this).remove();
+    }
+});
 
 fs.writeFile('./index.html', $.html(), {encoding: 'utf8'}, function(err) {
     if (err) {
